b1e54c170389359c3ad9b8214baa29cb5bae49b0
andre souza <andre.souza@creathus.org.br>
2023-04-05 14:45:03 -0400

    add middlewares, uuid, editando reame

--- Readme.md
+++ Readme.md
@@ -1,9 +1,11 @@
-# FinApi - Financeia
+## FinApi - Financeia
 
-## Requisitos
+### Requisitos
 
-- [ ] Deve ser possível criar umma conta;
-- [ ] Deve ser possível buscar o extrato bancário do cliente;
+---
+
+- [x] Deve ser possível criar umma conta;
+- [x] Deve ser possível buscar o extrato bancário do cliente;
 - [ ] Deve ser possível realizar um depósito;
 - [ ] Deve ser possível realizar um saque;
 - [ ] Deve ser possível buscar o extrato bancário do cliente por data;
@@ -11,10 +13,12 @@
 - [ ] Deve ser possível obter dados da conta do cliente;
 - [ ] Deve ser possível deletar uma conta.
 
-## Regras de negócio
+---
 
-- [ ] Não deve ser possível cadastrar uma conta com CPF já existente
-- [ ] Não deve ser possível fazer depósito em uma conta não existente
+### Regras de negócio
+
+- [x] Não deve ser possível cadastrar uma conta com CPF já existente
+- [x] Não deve ser possível fazer depósito em uma conta não existente
 - [ ] Não deve ser possível buscar extrato em uma conta não existente
 - [ ] Não deve ser possível fazer saque em uma conta não existente
 - [ ] Não deve ser possível excluir uma conta não existente
--- src/index.js
+++ src/index.js
@@ -1,42 +1,61 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-
 app.use(express.json());
 
+const costumers = [];
+
+// Middleware
+const verifyIfExistsAccountCPF = (request, response, next) => {
+  const { cpf } = request.headers;
+  const costumer = costumers.find((costumer) => costumer.cpf === cpf);
+  if (!costumer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+  request.costumer = costumer;
+  return next();
+};
+
+// forma de implementar o middleware em todas as rotas da aplicação
+// app.use(verifyIfExistsAccountCPF);
+
 /**
- * GET    = Buscar uma informação dentro do servidor;
- * POST   = Inserir uma informação no servidor;
- * PUT    = Alterar uma informação no servidor;
- * DELETE = Deletar uma informação no servidor;
- * PATCH  = Alterar uma informação específica;
- *
- * TIPOS DE PARÂMETROS:
- * route params => http://localhost:3333/produtos/123456789
- * query params => http://localhost:3333/produtos?name=teclado&description=teclado%20gamer
- * body params  => {
- * "name": "teclado",
- * "description": "teclado gamer"
- * }
+ * @method POST
+ * @param cpf        = string;
+ * @param name       = string;
+ * @param id         = uuid;
+ * @param statement  = [];
  */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
 
-app.get("/cursos", (request, response) => {
-  const query = request.query;
-  console.log(query);
-  return response.status(200).json({ message: "Curso criado com sucesso!" });
+  const customerAlreadyExists = costumers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  costumers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).json({ message: "Customer created!" });
 });
 
-app.post("/cursos", (request, response) => {
-  const body = request.body;
-  console.log(body);
-  return response.status(201).json({ message: "Curso criado com sucesso!" });
+/**
+ * @method GET
+ * @param {headers} cpf = string;
+ */
+// forma de implementar o middleware em uma rota específica
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { costumer } = request;
+  return response.json(costumer.statement);
 });
 
-app.put("/cursos/:id", (request, response) => {
-  const { id } = request.params;
-  console.log(`O id selecionado foi: `, id);
-  return response.status(200).json({ message: `O ID selecionado foi: ${id}` });
-});
-
-// localhost:3333
 app.listen(3333);
